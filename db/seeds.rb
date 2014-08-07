f3c7fbe60fe7c83f2db2f5bab67c549a4c8d8759
Your Name <you@example>
2014-08-07 14:56:52 +0000

    hiseeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,5 @@ Question.create!([
 Quiz.create!([
   {chapter_number: 1, chapter_name: "Introduction to Geometry", number_of_questions: 5, user_id: 2, score: nil, number_answered: nil, completed: nil, correct_questions: nil}
 ])
-
+User.create!([
+  {name: "Tobe", email: "tobochi2003@gmail.com", bio: "Coder, Swimmer, Pianist, Maker of this Site", pic: "http://e08595.medialib.glogster.com/media/e4/e495ca8daec7b27c8e5bdee60171b38631b71882200a8a29be167ff88caeb122/735909d4517dd536d0375ab09d8a6b19-png.png", password_digest: "$2a$10$JG.gtN6IkgAWtpStop6LReYS1bu79Hlxp/8JondHSrYTlGotjnbSu"},
